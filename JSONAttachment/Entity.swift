12b75028c6e91f1a5481750f0b663f0752724ea8
Christian Tietze <christian.tietze@gmail.com>
2020-02-21 10:33:27 +0100

    add EntityWriter

--- JSONAttachment/Entity.swift
+++ JSONAttachment/Entity.swift
@@ -1,6 +1,6 @@
 //  Copyright © 2020 Christian Tietze. All rights reserved. Distributed under the MIT License.
 
-public struct Identifier<E> where E: Entity {
+public struct Identifier<E: Entity>: Equatable, Hashable {
     public let rawValue: String
 
     public init?(rawValue: String) {
@@ -8,6 +8,6 @@ public struct Identifier<E> where E: Entity {
     }
 }
 
-public protocol Entity {
+public protocol Entity: Codable {
     var identifier: Identifier<Self> { get }
 }
---
+++ JSONAttachment/EntityWriter.swift
@@ -0,0 +1,17 @@
+//  Copyright © 2020 Christian Tietze. All rights reserved. Distributed under the MIT License.
+
+public final class EntityWriter<E: Entity> {
+    public let baseURL: URL
+
+    public init(baseURL: URL) {
+        self.baseURL = baseURL
+    }
+
+    lazy var encoder = JSONEncoder()
+
+    public func write(entity: E) throws {
+        let data = try encoder.encode(entity)
+        let url = baseURL.appendingPathComponent(entity.identifier.rawValue)
+        try data.write(to: url, options: .atomicWrite)
+    }
+}
