823b5c75860e030e4c037899c34c97d88c48a09e
Christian Tietze <christian.tietze@gmail.com>
2020-02-21 09:51:54 +0100

    Initial Commit

---
+++ JSONAttachment/JSONAttachment.h
@@ -0,0 +1,19 @@
+//
+//  JSONAttachment.h
+//  JSONAttachment
+//
+//  Created by Christian on 21.02.20.
+//  Copyright © 2020 Christian Tietze. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for JSONAttachment.
+FOUNDATION_EXPORT double JSONAttachmentVersionNumber;
+
+//! Project version string for JSONAttachment.
+FOUNDATION_EXPORT const unsigned char JSONAttachmentVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JSONAttachment/PublicHeader.h>
+
+
