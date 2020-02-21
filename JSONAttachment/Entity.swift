dddf83cbdd02e47cccee47d834392470e83bcb13
Christian Tietze <christian.tietze@gmail.com>
2020-02-21 16:46:52 +0100

    change Identifier initializer

--- JSONAttachment/Entity.swift
+++ JSONAttachment/Entity.swift
@@ -3,7 +3,7 @@
 public struct Identifier<E: Entity>: Equatable, Hashable {
     public let rawValue: String
 
-    public init?(rawValue: String) {
+    public init(_ rawValue: String) {
         self.rawValue = rawValue
     }
 }
